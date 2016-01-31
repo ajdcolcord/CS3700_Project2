5f8af00acbf83f973248c60e3706371b7b1c5023
Austin Colcord <ajdcolcord@gmail.com>
2016-01-31 15:55:06 -0500

    Successfull trial connection with lan.py and BPDU.py, message being sent to lan.py from BPDU.py

--- BPDU.py
+++ BPDU.py
@@ -1,27 +1,23 @@
 import socket
-import traceback
 import time
 
-host = ''
 
-port = 50000
+def broadcast_bpdu():
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-s.bind((host, port))
+    host = ''
 
-while 1:
-    data = repr(time.time()) + '\n'
-    s.sendto(data, ('<broadcast>', port))
+    for x in range(10000, 65536):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        print "Host", host
+        print "Port", x
+        try:
+            s.bind((host, x))
+        except:
+            print "Already in use"
+
+        data = "Time" + repr(time.time()) + '\n'
+        s.sendto(data, ('<broadcast>', x))
+
     time.sleep(1)
-    '''
-    try:
-        message, address = s.recvfrom(8192)
-        print "GOT DATA FROM : ", address
-        s.sendto("I am here", address)
-    except (KeyboardInterrupt, SystemExit):
-        raise
-    except:
-        traceback.print_exc()
-    '''
--- lan.py
+++ lan.py
@@ -1,1 +1,6 @@
-class LAN: 
+from socket import *
+
+s = socket(AF_INET, SOCK_DGRAM)
+s.bind(('', 12345))
+m = s.recvfrom(1024)
+print m[0]
