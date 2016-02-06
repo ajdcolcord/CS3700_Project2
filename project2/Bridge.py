fb0ad38ecc068d57412baa3b9a8620c16af9e955
Austin Colcord <ajdcolcord@gmail.com>
2016-02-06 10:08:16 -0500

    starting to add BPDU functionality

--- project2/Bridge.py
+++ project2/Bridge.py
@@ -3,7 +3,6 @@
 import sys
 import socket
 import select
-import json
 from Packet import Packet
 
 RECEIVE_SIZE = 1500
@@ -14,6 +13,8 @@ class Bridge:
         self.id = bridgeID
         self.LAN_list = LAN
         self.sockets = []
+        self.rootID = self.id
+
         self._create_sockets_for_lans()
         self._start_receiving()
 
@@ -28,13 +29,13 @@ class Bridge:
         while True:
             # Calls select with all the sockets; change the timeout value (1)
             ready, ignore, ignore2 = select.select(self.sockets, [], [], 1)
-
             # Reads from each fo the ready sockets
             for x in ready:
                 message = x.recv(RECEIVE_SIZE)
-
                 # create new packet object from the incoming message
                 packet = Packet(message)
+                if packet.isBPDU:
+                    self._choose_rootID_from_BPDU(packet)
 
     def _pad(self, name):
         # pads the name with null bytes at the end
@@ -42,3 +43,25 @@ class Bridge:
         while len(result) < 108:
                 result += '\0'
         return result
+
+    def _choose_rootID_from_BPDU(self, BPDU_in):
+        rootID_2 = BPDU_in.rootID
+        cost_2 = BPDU_in.cost
+        bridgeID_2 = BPDU_in.id
+
+    # bridge logic:
+    # all bridges first assume they are the root
+    # for each received BPDU, the switch chooses:
+    #     - a new root (smallest known ROOT ID)
+    #     - a new root port (the port that points toward that root)
+    #     - a new designated bridge (who is the next hop to root)
+    # DO THIS BY USING LOGIC:
+    #  - if ROOT ID1 < ROOT ID2: use BPDU-1
+    #  - else if ROOT ID1 == ROOT ID2 && COST1 < COST2: use BPDU-1
+    #  - else if ROOT ID1 == ROOT ID2 && COST1 == COST2 && BRIDGE-ID1 < BRIDGE-ID2: use BPDU-1
+    #  - else: use BPDU-2
+
+    # BPDU logic:
+    # elect root Bridge
+    # locate next hop closest to root, and it's port
+    # select ports to be included in spanning trees, disable others
--- project2/Packet.py
+++ project2/Packet.py
@@ -6,6 +6,7 @@ import json
 class Packet():
     def __init__(self, message):
         self._parse_to_Packet(message)
+        self.isBPDU = False
 
     def _parse_to_Packet(self, message):
         json_message = json.loads(message)
@@ -13,7 +14,7 @@ class Packet():
         self.dest = json_message['dest']
         self.type = json_message['type']
         self.message = json_message['message']
-        self.messageID = self.message['id']
+        self.id = self.message['id']
 
         print "Source: ", self.source
         print "Dest: ", self.dest
@@ -21,20 +22,12 @@ class Packet():
         print "Message: ", self.message
         print "MessageID: ", self.messageID
 
+        if self.type == 'bpdu':
+            self._create_BPDU(self.message)
+            self.isBPDU = True
 
-    #bridge logic:
-    # all bridges first assume they are the root
-    # for each received BPDU, the switch chooses:
-    #     - a new root (smallest known ROOT ID)
-    #     - a new root port (the port that points toward that root)
-    #     - a new designated bridge (who is the next hop to root)
-    # DO THIS BY USING LOGIC:
-    #  - if ROOT ID1 < ROOT ID2: use BPDU-1
-    #  - else if ROOT ID1 == ROOT ID2 && COST1 < COST2: use BPDU-1
-    #  - else if ROOT ID1 == ROOT ID2 && COST1 == COST2 && BRIDGE-ID1 < BRIDGE-ID2: use BPDU-1
-    #  - else: use BPDU-2
-
-    # BPDU logic:
-    # elect root Bridge
-    # locate next hop closest to root, and it's port
-    # select ports to be included in spanning trees, disable others
+    def _create_BPDU(self, message):
+        self.rootID = message['root']
+        self.cost = message['cost']
+        print "RootID: ", self.rootID
+        print "Cost: ", self.cost
