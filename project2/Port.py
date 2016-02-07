3b53f200a0696c56d89d25a56bc57b79e94cae62
Austin Colcord <ajdcolcord@gmail.com>
2016-02-07 10:07:33 -0500

    added comments, split up functions to simplify

--- project2/Port.py
+++ project2/Port.py
@@ -5,13 +5,27 @@ import time
 
 
 class Port:
+    """
+    This is the class for a Port, which contains a portID, enabled status,
+    list of BPDU's (sorted by 'bestness'), and a socket (referenced)
+    """
     def __init__(self, port_id, socket=None, enabled=True, BPDU_list=[]):
+        """
+        initializes a new port, with the given sock ID. a socket input is
+        defaulted to None, enabled is defaulted to True, and the BPDU_list
+        is defaulted to an empty array
+        """
         self.port_id = port_id
         self.enabled = enabled
         self.BPDU_list = BPDU_list
         self.socket = socket
 
     def add_BPDU(self, BPDU):
+        """
+        This function adds the given BPDU into this BPDU_list based on
+        'bestness'. It also clears out any expired BPDUs that have timed out
+        @param BPDU : the BPDU to add
+        """
         iterator = 0
         bpdu_added = False
         BPDU.time = time.time()
@@ -20,16 +34,39 @@ class Port:
             bpdu_added = True
         for bpdu in self.BPDU_list:
 
-            # If the bpdu has timed out, simply remove it
-            if int(round((time.time() - bpdu.time) * 1000)) > 750:
-                self.BPDU_list.remove(bpdu)
+            self._remove_timedout_BPDU(bpdu)
 
             if bpdu.is_incoming_BPDU_better(BPDU) and not bpdu_added:
-                self.BPDU_list.insert(iterator, BPDU)
-                bpdu_added = True
-                print "ADDED BPDU TO PORT: ", self.port_id
+                bpdu_added = self._add_BPDU_at_position(BPDU, iterator)
+                #self.BPDU_list.insert(iterator, BPDU)
+                #bpdu_added = True
+                # print "ADDED BPDU TO PORT: ", self.port_id
 
             iterator += 1
 
+        # if the BPDU has not been added yet, add it at the end
         if not bpdu_added:
             self.BPDU_list.append(BPDU)
+
+    def _remove_timedout_BPDU(self, BPDU):
+        """
+        Checks if the given BPDU has timed out. If so, remove it
+        from this BPDU list
+        @param BPDU : the BPDU to check
+        """
+        # If the bpdu has timed out, simply remove it
+        if int(round((time.time() - BPDU.time) * 1000)) > 750:
+            self.BPDU_list.remove(BPDU)
+
+    def _add_BPDU_at_position(self, BPDU, position):
+        """
+        Adds the given BPDU at the given position in this BPDU list
+        and returns true after it is added
+        @param BPDU : the BPDU to add into this BPDU list
+        @param position : the position to add the BPDU in the list
+        @return True
+        """
+        self.BPDU_list.insert(position, BPDU)
+        bpdu_added = True
+        print "ADDED BPDU TO PORT: ", self.port_id
+        return True
