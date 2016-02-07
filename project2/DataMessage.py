05feddd94b08e30ab27807b66fe676e58e4e9849
Austin Colcord <ajdcolcord@gmail.com>
2016-02-07 10:05:33 -0500

    Added comments to DataMessage and fixed some found bugs

--- project2/DataMessage.py
+++ project2/DataMessage.py
@@ -3,7 +3,17 @@ import json
 
 
 class DataMessage:
-    def __init__(self, source, dest, ID, cost):
+    """
+    This is the DataMessage class for messages that are meant to be Sent
+    from host to host
+    """
+    def __init__(self, source, dest, ID):
+        """
+        This creates a new DataMessage based on the provided information
+        @param source : the source address
+        @param dest : the destination address
+        @param ID : the message ID
+        """
         self.source = source
         self.dest = dest
         self.type = 'data'
@@ -18,7 +28,7 @@ class DataMessage:
         json_DataMessage_object = {}
         json_DataMessage_object['source'] = self.source
         json_DataMessage_object['dest'] = self.dest
-        json_DataMessage_object['type'] = 'bpdu'
+        json_DataMessage_object['type'] = 'data'
 
         DataMessage_message_object = {}
         DataMessage_message_object['id'] = self.id
@@ -29,8 +39,15 @@ class DataMessage:
 
 
 def create_DataMessage_from_json(json_message):
+    """
+    This function takes in a json_message, checks if it's type is 'data',
+    if so, creates a new DataMessage object from it's contained information
+    @param json_message : the json message to create an object from
+    @return DataMessage from the provided message's information, returns False
+    if the type is not 'data'
+    """
     json_obj = json.loads(json_message)
-    if json_obj['type'] == 'bpdu':
+    if json_obj['type'] == 'data':
         src = json_obj['source']
         dest = json_obj['dest']
 
