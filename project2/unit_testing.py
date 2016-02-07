69b19667f725133918803f77069bf06d092390f0
Austin Colcord <ajdcolcord@gmail.com>
2016-02-07 10:04:27 -0500

    added unit testing for DataMessage

--- project2/unit_testing.py
+++ project2/unit_testing.py
@@ -6,6 +6,7 @@ from Bridge import Bridge
 from Packet import Packet
 from ForwardingTable import ForwardingTable
 from BPDU import BPDU, create_BPDU_from_json
+from DataMessage import DataMessage, create_DataMessage_from_json
 
 
 class TestBPDUMethods(unittest.TestCase):
@@ -32,8 +33,6 @@ class TestBPDUMethods(unittest.TestCase):
 
         json_test_message = testBPDU.create_json_BPDU()
         json_loaded_message = json.loads(json_test_message)
-        #print json_test_message
-
         self.assertEquals(json_loaded_message['source'], testBPDU.source)
         self.assertEquals(json_loaded_message['dest'], testBPDU.dest)
         self.assertEquals(json_loaded_message['type'], testBPDU.type)
@@ -41,19 +40,41 @@ class TestBPDUMethods(unittest.TestCase):
         self.assertEquals(json_loaded_message['message']['root'], testBPDU.root)
         self.assertEquals(json_loaded_message['message']['cost'], testBPDU.cost)
 
-    def test_is_incoming_BPDU_root(self):
+    def test_is_incoming_BPDU_better(self):
         testBPDU1 = BPDU('0ab2', 'ffff', '98b2', '0ab2', 4)
         testBPDU2 = BPDU('91b5', 'ffff', '98b3', '0ab2', 4)
         testBPDU3 = BPDU('0ab2', 'ffff', '98b2', '0ab5', 4)
         testBPDU4 = BPDU('0ab2', 'ffff', '98b2', '0ab2', 6)
 
+        self.assertFalse(testBPDU1.is_incoming_BPDU_better(testBPDU2))
+        self.assertFalse(testBPDU1.is_incoming_BPDU_better(testBPDU3))
+        self.assertFalse(testBPDU1.is_incoming_BPDU_better(testBPDU4))
+        self.assertTrue(testBPDU4.is_incoming_BPDU_better(testBPDU1))
 
 
-        self.assertFalse(testBPDU1.is_incoming_BPDU_root(testBPDU2))
-        self.assertFalse(testBPDU1.is_incoming_BPDU_root(testBPDU3))
-        self.assertFalse(testBPDU1.is_incoming_BPDU_root(testBPDU4))
-        self.assertTrue(testBPDU4.is_incoming_BPDU_root(testBPDU1))
+class TestDataMessageMethods(unittest.TestCase):
+    json_Data = '{"source":"02a1", "dest":"9xa2", "type": "data", "message":{"id":"92b4"}}'
 
+    def test_create_DataMessage_from_json(self):
+        testDataMessage = create_DataMessage_from_json(self.json_Data)
+        self.assertEquals(testDataMessage.source, '02a1')
+        self.assertEquals(testDataMessage.dest, '9xa2')
+        self.assertEquals(testDataMessage.type, 'data')
+        self.assertEquals(testDataMessage.id, '92b4')
+
+    def test_init_and_create_json_DataMessage(self):
+        testDataMessage = DataMessage('02a1', '9xa2', '98b2')
+        self.assertEquals(testDataMessage.source, '02a1')
+        self.assertEquals(testDataMessage.dest, '9xa2')
+        self.assertEquals(testDataMessage.type, 'data')
+        self.assertEquals(testDataMessage.id, '98b2')
+
+        json_test_message = testDataMessage.create_json_DataMessage()
+        json_loaded_message = json.loads(json_test_message)
+        self.assertEquals(json_loaded_message['source'], testDataMessage.source)
+        self.assertEquals(json_loaded_message['dest'], testDataMessage.dest)
+        self.assertEquals(json_loaded_message['type'], testDataMessage.type)
+        self.assertEquals(json_loaded_message['message']['id'], testDataMessage.id)
 
 
 
