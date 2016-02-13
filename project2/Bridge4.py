ee6e6d93814c6a3cbf1bb3ba2a41ea14c68d7d1f
Austin Colcord <ajdcolcord@gmail.com>
2016-02-13 12:29:51 -0500

    increasing timeout again

--- project2/Bridge4.py
+++ project2/Bridge4.py
@@ -83,8 +83,7 @@ class Bridge:
                 start_time = time.time()
 
             print "BRIDGE " + str(self.id) + ": ROOT = " + str(self.bridge_BPDU.root) + " ON PORT: " + str(self.rootPort_ID) + " WITH COST: " + str(self.bridge_BPDU.cost)
-            ready, ignore, ignore2 = select.select([p.socket for p in self.ports], [], [], 0.01)
-
+            ready, ignore, ignore2 = select.select([p.socket for p in self.ports], [], [], 0.1)
 
             for port in self.ports:
                 if not port.BPDU_list:
