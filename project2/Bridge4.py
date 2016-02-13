2ef95db8a601713f29a1599b0567bf62f478573f
Austin Colcord <ajdcolcord@gmail.com>
2016-02-13 10:38:23 -0500

    adding bpdu to port

--- project2/Bridge4.py
+++ project2/Bridge4.py
@@ -135,11 +135,17 @@ class Bridge:
                 if changed_root_id:
                     self._print_root_port(self.rootPort_ID)
 
+                # -------NEW------------
+                port_in.add_BPDU(bpdu_in)
+                # ---------------------
+
                 # broadcast new information about the bridge
                 self._broadcast_BPDU()
             else:
                 print "THIS THINKS IT's THE ROOT: INCOMING NOT BETTER"
         else:
+            # TODO: WHAT IF PORT LIST IS EMPTY
+
             if port_in.BPDU_list[0].is_incoming_BPDU_better(bpdu_in):
                 print "THIS THINKS IT's NOT THE ROOT BRIDGE: INCOMING BPDU BETTER THAN ON PORT"
 
@@ -158,6 +164,10 @@ class Bridge:
                     if changed_root_id:
                         self._print_root_port(self.rootPort_ID)
 
+                    # -------NEW------------
+                    port_in.add_BPDU(bpdu_in)
+                    # ---------------------
+
                     # broadcast new information about the bridge
                     self._broadcast_BPDU()
 
@@ -170,9 +180,17 @@ class Bridge:
                     #if the designated status used to be false, print designated
                     if not previous_designation:
                         self._print_designated_port(port_in.port_id)
+
+                    # -------NEW------------
+                    port_in.add_BPDU(bpdu_in)
+                    # ---------------------
             else:
                 print "THIS THINK's IT's NOT THE ROOT: INCOMING NOT BETTER THAN PORT"
                 port_in.designated = False
+
+                # -------NEW------------
+                port_in.add_BPDU(bpdu_in)
+                # ---------------------
 
         self._enable_or_disable(port_in)
 
