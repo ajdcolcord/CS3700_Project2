a1e437a1f0f3da91983d9f3d1b51d07512fe8cd6
Austin Colcord <ajdcolcord@gmail.com>
2016-02-13 12:21:38 -0500

    testing out

--- project2/Bridge4.py
+++ project2/Bridge4.py
@@ -83,7 +83,7 @@ class Bridge:
                 start_time = time.time()
 
             print "BRIDGE " + str(self.id) + ": ROOT = " + str(self.bridge_BPDU.root) + " ON PORT: " + str(self.rootPort_ID) + " WITH COST: " + str(self.bridge_BPDU.cost)
-            ready, ignore, ignore2 = select.select([p.socket for p in self.ports], [], [], 0.1)
+            ready, ignore, ignore2 = select.select([p.socket for p in self.ports], [], [], 0.5)
 
 
             for port in self.ports:
