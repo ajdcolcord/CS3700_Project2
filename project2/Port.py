6368a5d76ac3c2ffee300ff8f42712901efb79e7
Austin Colcord <ajdcolcord@gmail.com>
2016-02-16 14:51:20 -0500

    oops

--- project2/Port.py
+++ project2/Port.py
@@ -9,7 +9,7 @@ class Port:
     This is the class for a Port, which contains a portID, enabled status,
     list of BPDU's (sorted by 'bestness'), and a socket (referenced)
     """
-    def __init__(self, port_id, socket=None, enabled=True, BPDU_list=[]):
+    def __init__(self, port_id, socket=None, enabled=True):
         """
         initializes a new port, with the given sock ID. a socket input is
         defaulted to None, enabled is defaulted to True, and the BPDU_list
@@ -17,7 +17,7 @@ class Port:
         """
         self.port_id = port_id
         self.enabled = enabled
-        self.BPDU_list = BPDU_list
+        self.BPDU_list = []
         self.socket = socket
         self.designated = True
 
