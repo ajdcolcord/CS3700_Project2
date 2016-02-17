8dfeb794135e2c3bfed704d603254763b5bfd5e5
Austin Colcord <ajdcolcord@gmail.com>
2016-02-17 17:08:35 -0500

    null check

--- project2/Bridge4.py
+++ project2/Bridge4.py
@@ -80,7 +80,7 @@ class Bridge:
                 best_port = best_bpdu_list[0][0]
 
                 #if self.bridge_BPDU.is_incoming_BPDU_better(best_bpdu):
-                if self.ports[self.rootPort_ID].is_incoming_BPDU_better(best_bpdu):
+                if not len(self.ports[self.rootPort_ID]) or self.ports[self.rootPort_ID].is_incoming_BPDU_better(best_bpdu):
                     self._change_root(best_port, best_bpdu)
 
             # find the best bpdu (of all ports), if it has changed (meaning root (bridge's bpdu) will now have to change), then set it
