a4ba16b9391bd028514494c1456df6c4c2f09603
Austin Colcord <ajdcolcord@gmail.com>
2016-02-17 17:37:33 -0500

    more tests

--- project2/Bridge4.py
+++ project2/Bridge4.py
@@ -80,8 +80,8 @@ class Bridge:
                 best_port = best_bpdu_list[0][0]
 
                 #if self.bridge_BPDU.is_incoming_BPDU_better(best_bpdu):
-                if self.rootPort_ID is None or not len(self.ports[self.rootPort_ID].BPDU_list) or self.ports[self.rootPort_ID].BPDU_list[0].is_incoming_BPDU_better(best_bpdu):
-                    print "ROOT NOT SET: " + str(self.rootPort_ID) + " NOT ROOT BPDUS: " + str(not len(self.ports[self.rootPort_ID].BPDU_list)) + " INCOMING BETTER " + str(self.ports[self.rootPort_ID].BPDU_list[0].is_incoming_BPDU_better(best_bpdu))
+                if self.rootPort_ID is None or not len(self.ports[self.rootPort_ID].BPDU_list) or best_bpdu.BPDU_list[0].is_incoming_BPDU_better(self.ports[self.rootPort_ID]):
+                    print "ROOT NOT SET: " + str(self.rootPort_ID is None) + " NOT ROOT BPDUS: " + str(not len(self.ports[self.rootPort_ID].BPDU_list)) + " INCOMING BETTER " + str(self.ports[self.rootPort_ID].BPDU_list[0].is_incoming_BPDU_better(best_bpdu))
                     self._print_bridge_info()
                     self._change_root(best_port, best_bpdu)
                     self._print_bridge_info()
